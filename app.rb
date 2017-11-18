2f23020444164771ef07181100e37000547f9b21
Ezra M. Chang <ezra.chang@3dresults.com>
2017-11-17 23:46:35 -0600

    Found an error in logic.

--- app.rb
+++ app.rb
@@ -3,7 +3,8 @@ require_relative "rf_controller"
 rfc = RFController.new
 while true do
     print "Which switch? "
-    input = gets.chomp.split(" ")
-    rfc.sigOut(input[0], input[1])
+    input = gets.chomp
     break if input == "q"
+    parsed = input.split(" ")
+    rfc.sigOut(parsed[0], parsed[1])
 end
